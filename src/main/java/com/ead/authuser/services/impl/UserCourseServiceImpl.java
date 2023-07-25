64ff97cc3e904f29edbb4242b4ce456cb843acf3
jaderson.brandao <jaderson.brandao@gmail.com>
2023-07-25 10:32:38 -0300

    Mapear Relacionamentos para Comunicação entre Microservices

--- src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -1,10 +1,11 @@
 package com.ead.authuser.services.impl;
 
 import com.ead.authuser.repositories.UserCourseRepository;
+import com.ead.authuser.services.UserCourseService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserCourseServiceImpl {
+public class UserCourseServiceImpl implements UserCourseService {
 
     private final UserCourseRepository userCourseRepository;
 
