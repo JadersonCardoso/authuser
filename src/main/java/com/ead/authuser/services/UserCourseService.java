8c1333380bdfac783f18c1b9614d8b07bbbdbaad
jaderson.brandao <jaderson.brandao@gmail.com>
2023-08-08 08:51:41 -0300

    Comunicação Síncrona - Inscrição de User em Course - Parte 4

--- src/main/java/com/ead/authuser/services/UserCourseService.java
+++ src/main/java/com/ead/authuser/services/UserCourseService.java
@@ -1,4 +1,13 @@
 package com.ead.authuser.services;
 
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
+
+import java.util.UUID;
+
 public interface UserCourseService {
+
+    boolean existsByUserAndCouseId(UserModel userModel, UUID courseId);
+
+    UserCourseModel save(UserCourseModel useCourseModel);
 }
--- src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.services.UserCourseService;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserCourseServiceImpl implements UserCourseService {
@@ -13,5 +17,13 @@ public class UserCourseServiceImpl implements UserCourseService {
         this.userCourseRepository = userCourseRepository;
     }
 
+    @Override
+    public boolean existsByUserAndCouseId(UserModel userModel, UUID courseId) {
+        return userCourseRepository.existsByUserAndCourseId(userModel, courseId);
+    }
 
+    @Override
+    public UserCourseModel save(UserCourseModel useCourseModel) {
+        return userCourseRepository.save(useCourseModel);
+    }
 }
